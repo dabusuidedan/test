217a1e26733b36592a288d126aaa0172088c3a62
朱科 <439366720@qq.com>
2019-01-26 23:09:19 +0800

    json

--- src/main/java/com/zk/controller/IndexController.java
+++ src/main/java/com/zk/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.zk.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/home")
@@ -11,5 +13,12 @@ public class IndexController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+
+    @RequestMapping("json")
+    @ResponseBody
+    public  String second(@RequestBody String jsonText){
+        return jsonText;
     }
 }
